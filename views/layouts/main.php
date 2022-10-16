0a63fc675aaa6085329bc9c185e6c76ddd212861
lee <lmaucoin@gmail.com>
2022-10-16 20:42:09 +0000

    Adding OG markup

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,6 +17,9 @@ $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
 $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerMetaTag(['name' => 'twitter:card', 'content' => 'summary']);
+$this->registerMetaTag(['name' => 'og:title', 'content' => 'NOLADIY.ORG - New Orleans Events, Bands, Venues']);
+$this->registerMetaTag(['name' => 'og:image', 'content' => 'https://noladiy.org/nola.jpg']);
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => '@web/favicon.ico']);
 
 $loggedIn = (!Yii::$app->user->isGuest);
